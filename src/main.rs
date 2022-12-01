a93996533a822d4e489b4bc8baf068b4311ccd70
Cain <75994858+cainthebest@users.noreply.github.com>
2022-12-01 19:19:28 +0100

    chore(main): tidy output

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,9 @@ use crate::day01::{part_one, part_two};
 mod day01;
 
 fn day01() {
-    println!("Day 01 - Part 1: {}", part_one());
-    println!("Day 01 - Part 2: {}", part_two());
+    println!("---- Day 1 ----");
+    println!("Part 1: {}", part_one());
+    println!("Part 2: {}", part_two());
 }
 
 fn main() {
